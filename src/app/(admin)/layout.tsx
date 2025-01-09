4ee2881c38f6044207008392d13ed24b2b886c18
Asad Bigfolio <asad@bigfolio.co>
2025-01-09 13:38:58 +0500

    Update layout.tsx

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,14 +1,14 @@
 import React, { ReactNode } from 'react';
-import AppLayout from '@/Components/userLayout';
-import loadSession from '@/utils/session';
+// import AppLayout from '@/Components/userLayout';
+// import loadSession from '@/utils/session';
 
 interface LayoutProps {
     children: ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = async ({ children }) => {
-    const session = await loadSession()
-    return <AppLayout session={session}>{children}</AppLayout>;
+    // const session = await loadSession()
+    return <>{children}</>;
 };
 
 export default Layout;
